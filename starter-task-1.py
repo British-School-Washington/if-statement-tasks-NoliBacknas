173ef0e5389ff4284839b3e338dfaf75875c1e67
NoliBacknas <noli_backnas@biswashington.org>
2024-11-21 17:29:22 +0000

    Hello, done

--- starter-task-1.py
+++ starter-task-1.py
@@ -2,13 +2,14 @@
 number = int(input("Enter an integer: "))
 
 # if the number is positive
-if number > 0:
-    # TODO: print positive message here
+if number > 0: 
+    print("number is posetive")
 
 # if the number is negative
 elif number < 0:
-    # TODO: print negative message here
+    print("number is negative")
+  
 
 # if the number is zero
 else:
-    # TODO: print zero message here
+    print("nuber is zero")
